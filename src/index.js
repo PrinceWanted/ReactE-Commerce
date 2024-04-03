2d0177c138a1f9fee6491a6909df458c7ddac4c1
PrinceWanted <56977987+PrinceWanted@users.noreply.github.com>
2024-04-03 16:26:32 +0200

    Finished user Authentication/Protecting Checkout route

--- src/index.js
+++ src/index.js
@@ -10,12 +10,26 @@ import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//domain: dev-bddjqiq551usfntf.eu.auth0.com
+//client ID: iaUPthTXK306Zs9c5NjPoEKvo9ToDAly
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-bddjqiq551usfntf.eu.auth0.com" //"dev-bddjqiq551usfntf.eu.auth0.com"
+    clientId="iaUPthTXK306Zs9c5NjPoEKvo9ToDAly" //"HOxOgoGovmCLVJu8zwCO5u0GyhsZHmqN"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
