7dc57f8c27d8df07d7afbe17539c7c66b63dca52
PrinceWanted <56977987+PrinceWanted@users.noreply.github.com>
2024-04-24 15:05:43 +0200

    Design Edits

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import heroBcg from "../assets/hero-bcg.jpeg";
-import heroBcg2 from "../assets/hero-bcg-2.jpeg";
+// import heroBcg from "../assets/hero-bcg.jpeg";
+// import heroBcg2 from "../assets/hero-bcg-2.jpeg";
+import heroBcg from "../assets/hero-bcg.svg";
 
 const Hero = () => {
   return (
     <Wrapper className="section-center">
       <article className="content">
         <h1>
-          design your <br />
-          comfort zone
+          All the Things <br />a Gamer Enjoys
         </h1>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure
@@ -23,7 +23,7 @@ const Hero = () => {
       </article>
       <article className="img-container">
         <img src={heroBcg} alt="nice table" className="main-img" />
-        <img src={heroBcg2} alt="working person" className="accent-img" />
+        {/* <img src={heroBcg2} alt="working person" className="accent-img" /> */}
       </article>
     </Wrapper>
   );
@@ -47,7 +47,7 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     height: calc(100vh - 5rem);
     grid-template-columns: 1fr 1fr;
-    gap: 8rem;
+    gap: 2rem;
     h1 {
       margin-bottom: 2rem;
     }
@@ -78,16 +78,16 @@ const Wrapper = styled.section`
       transform: translateX(-50%);
       border-radius: var(--radius);
     }
-    .img-container::before {
-      content: "";
-      position: absolute;
-      width: 10%;
-      height: 80%;
-      background: var(--clr-primary-9);
-      bottom: 0%;
-      left: -8%;
-      border-radius: var(--radius);
-    }
+    // .img-container::before {
+    //   content: "";
+    //   position: absolute;
+    //   width: 10%;
+    //   height: 80%;
+    //   background: var(--clr-primary-9);
+    //   bottom: 0%;
+    //   left: -8%;
+    //   border-radius: var(--radius);
+    // }
   }
 `;
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import Logo from "./Logo";
 import { Link } from "react-router-dom";
 import { useProductsContext } from "../context/products_context";
 import { FaTimes } from "react-icons/fa";
@@ -17,7 +18,7 @@ const Sidebar = () => {
         className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
       >
         <div className="sidebar-header">
-          <img src={logo} className="Logo" alt="E-Commerce Logo" />
+          <Logo/>
           <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes />
           </button>
